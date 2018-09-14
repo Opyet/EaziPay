0fafff10fe7dc538ccde1733874ae0b50524594d
Opeyemi Olabode <opeyemi_o@backlinktech.net>
2018-09-14 20:56:11 +0100

    Repo Initialized and Setup

---
+++ app/src/main/java/bts/eazipay/HomeScreenActivity.java
@@ -0,0 +1,19 @@
+package bts.eazipay;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+}
