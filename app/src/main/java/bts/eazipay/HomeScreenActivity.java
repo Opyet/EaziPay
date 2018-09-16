606d6e864df27886da94b96ed4421a8cde6d5a46
caseyadonie <caseyadonie@gmail.com>
2018-09-16 11:29:29 +0100

    All bugs fixed...changes integrated.

--- app/src/main/java/bts/eazipay/HomeScreenActivity.java
+++ app/src/main/java/bts/eazipay/HomeScreenActivity.java
@@ -6,20 +6,34 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
-    ImageView bttnReceivePay =null;
-    ImageView bttnDisbursePay =null;
+    LinearLayout bttnReceivePay =null;
+    LinearLayout bttnDisbursePay =null;
+
+    Button view_reports_button=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        bttnReceivePay = (ImageView)findViewById(R.id.bttnReceivePay);
-        bttnDisbursePay = (ImageView)findViewById(R.id.bttnDisbursePay);
+        bttnReceivePay = (LinearLayout)findViewById(R.id.bttnReceivePay);
+        bttnDisbursePay = (LinearLayout)findViewById(R.id.bttnDisbursePay);
+        view_reports_button=(Button)findViewById(R.id.view_reports_button) ;
 
+        view_reports_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent frmIntent= new Intent();
+                frmIntent.setClass(getApplicationContext() , HistoryActivity.class );
+
+                startActivity(frmIntent);
+            }
+        });
 
         bttnReceivePay.setOnClickListener(new View.OnClickListener() {
             @Override
